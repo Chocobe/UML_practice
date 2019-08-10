60b5306d3b083f442e5487c184a169ac9bf617f0
Chocobe <nice_10bird@naver.com>
2019-08-10 21:57:03 +0900

    <master> : Fixed comments

--- UML_practice/src/chapter_02_class/practice_02_02/Product.java
+++ UML_practice/src/chapter_02_class/practice_02_02/Product.java
@@ -1,11 +1,23 @@
 package chapter_02_class.practice_02_02;
 
+/*
+ * <2장. class> - 연습문제 2
+ * Product 클래스 설계하기
+ * 속성	-	productId	:	String	(제품번호)
+ * 		-	price		:	int		(가격)
+ * 
+ * 메소드	-	addProduct(productId : String) : boolean
+ * 		-	deleteProduct(productId : String) : boolean
+ * 		-	checkProduct(productId : String) : boolean
+ */
+
 public class Product {
 	private String productId;
 	private int price = 0;
 	
-	public void addProduct(String productId) {
+	public boolean addProduct(String productId) {
 		
+		return true;
 	}
 	
 	public boolean deleteProduct(String productId) {
