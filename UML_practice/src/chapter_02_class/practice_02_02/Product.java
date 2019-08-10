6e6d31d57256f2c5186285df06ea0a2edbafe307
Chocobe <nice_10bird@naver.com>
2019-08-10 21:12:55 +0900

    <master> : Created package practice_02_02

---
+++ UML_practice/src/chapter_02_class/practice_02_02/Product.java
@@ -0,0 +1,20 @@
+package chapter_02_class.practice_02_02;
+
+public class Product {
+	private String productId;
+	private int price = 0;
+	
+	public void addProduct(String productId) {
+		
+	}
+	
+	public boolean deleteProduct(String productId) {
+		
+		return true;
+	}
+	
+	private boolean checkProduct(String productId) {
+		
+		return true;
+	}
+}
