a2732ba3996099c82aa5663baf576e31500130e4
Chocobe <nice_10bird@naver.com>
2019-08-11 19:30:52 +0900

    <master> : Created practice_04_01

---
+++ UML_practice/src/chapter_04_capsulation/README.md
@@ -0,0 +1,3 @@
+# Capsulation (캡슐화)
+
+* practice_04_01 - Product UML을 코드로 작성하기
---
+++ UML_practice/src/chapter_04_capsulation/practice_04_01/Product.java
@@ -0,0 +1,19 @@
+package chapter_04_capsulation.practice_04_01;
+
+@SuppressWarnings("unused")
+public class Product {
+	private String productId;
+	private int price;
+	
+	public void addProduct() {
+		
+	}
+	
+	public void deleteProduct() {
+		
+	}
+	
+	public void checkProduct() {
+		
+	}
+}
